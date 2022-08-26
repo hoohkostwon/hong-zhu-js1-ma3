2a465559ac8bfcc3b899674461a6a7ef48342fca
hoohkostwon <100478012+hoohkostwon@users.noreply.github.com>
2022-08-26 16:10:45 +0200

    main

---
+++ js/question-2.js
@@ -0,0 +1,35 @@
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=30c81c6d5afd4b2297cea209487b0a4b";
+
+const resultContainer = document.querySelector (".results")
+
+function createHTML (content) {
+    resultContainer.innerHTML="";
+    for (let i=0; i< content.length; i++){
+        if (i===8) {
+            break
+        }
+        resultContainer.innerHTML+= `<div class="result">
+         Name: ${content[i].name} </br>
+         Rating: ${content[i].rating} </br>
+         Number of tags: ${content[i].tags.length}
+        </div>`
+    }
+}
+
+async function getUsers () {
+    try {
+        const response = await fetch (url);
+    const results= await response.json();
+    const content= results.results;
+    createHTML (content);
+    }
+    catch (error) {
+        resultContainer.innerHTML = displayError("An error occurred");
+    }
+}
+
+getUsers();
+
+function displayError (message= "Unkonwn error") {
+    return `<div class="eroor"> ${message}</div>`
+}
